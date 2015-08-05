30a36aeeded6b93872d9b6df9c70889f71e259b3
Planeshifter <pburckhardt@outlook.com>
2015-08-05 23:59:36 +0200

    [INIT]

---
+++ test/fixtures/test.array.jl
@@ -0,0 +1,24 @@
+using Distributions
+using JSON
+
+a = 0
+b = 1
+d = Uniform( a,b )
+
+x = linspace( .5, -100, 100 )
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("a", a),
+	("b", b),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/array.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
---
+++ test/fixtures/test.partial.jl
@@ -0,0 +1,24 @@
+using Distributions
+using JSON
+
+a = 0
+b = 1
+d = Uniform( a,b )
+
+x = [ -5, -2.5, 0, 2.5, 5 ]
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("a", a),
+	("b", b),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/partial.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
